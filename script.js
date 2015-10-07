cb66c0e64dec98513fd79e22a6f9b6e3416614a8
Lessar277 <lessar277@gmail.com>
2015-10-07 05:12:35 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+$("#submitbtn").click(function () {
+	$('#list').empty();
+	//split the list of names based on line breaks
+	var list = "";
+	var textAreaValue = $("#txtMessage").val();
+	var badNamesArray = textAreaValue.split("\n");
+	for (i = 0; i < badNamesArray.length; i++) {
+		var badName = badNamesArray[i].toLowerCase().split(" ");
+		var goodName = [];
+		for (j = 0; j < badName.length; j++) {
+			var firstChar = badName[j].charAt(0);
+			var x = badName[j].replace(firstChar, firstChar.toUpperCase());
+			goodName.push(x);
+		}
+		goodName = goodName.join(" ");
+		list += "<li>" + goodName + "</li>";
+
+	}
+	$('#list').append(list);
+});
