e6a9572acd0cec20251a13cbfc025d297b30de2f
Catalin Anghel <lessar277@gmail.com>
2015-10-20 18:44:27 +0300

    Update script.js
    Updated with regex to remove Mr/Ms/Miss/Mrs

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $("#submitbtn").click(function () {
 	var textAreaValue = $("#txtMessage").val();
 	var badNamesArray = textAreaValue.split("\n");
 	for (i = 0; i < badNamesArray.length; i++) {
-		var badName = badNamesArray[i].toLowerCase().split(" ");
+		var badName = badNamesArray[i].toLowerCase().replace(/\bm(iss|s|rs|r)\.?/gi, "").split(" ");
 		var goodName = [];
 		for (j = 0; j < badName.length; j++) {
 			var firstChar = badName[j].charAt(0);
